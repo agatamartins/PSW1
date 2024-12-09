218afa24409b93cef6d3279c4e4c5a7a91d0a8e1
agatamartins <149191964+agatamartins@users.noreply.github.com>
2024-12-09 19:52:54 -0300

    .

---
+++ psw1/atv03-agata-martins/home.php
@@ -0,0 +1,93 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" href="img/logo-psw.png" type="image/png">
+    <link rel="stylesheet" href="style.css">
+
+    <title>Site PSW</title>
+
+</head>
+<body>
+    <section>
+        <header>
+            <div class="logo-if">
+                <img src="img/logo-if.png" alt="">
+            </div>
+            <h1> SITE PROGRAMAÇÃO DE SISTEMAS WEB</h1>
+            <div class="logo-ceteia">
+                <img src="img/logo-ceteia.png" alt="">
+            </div>
+        </header>
+        <nav>
+            <div class="links">
+                <a href="https://ceteia.guanambi.ifbaiano.edu.br/pswmedio/2023/2ai/buffet/src/index.php" target="_blank">Buffet</a>
+                <a href="https://ceteia.guanambi.ifbaiano.edu.br/pswmedio/2023/2ai/cineview/cineview/index.php" target="_blank">Cineview</a>
+                <a href="https://ceteia.guanambi.ifbaiano.edu.br/pswmedio/2023/2ai/livraria/" target="_blank">Livraria</a>
+                <a href="https://ceteia.guanambi.ifbaiano.edu.br/pswmedio/2023/2ai/neurobiblioteca/" target="_blank">Neurobiblioteca</a>
+                <a href="https://ceteia.guanambi.ifbaiano.edu.br/pswmedio/2023/2ai/sisdoacao/" target="_blank">Doacao</a>
+                <a href="https://ceteia.guanambi.ifbaiano.edu.br/pswmedio/2023/2ai/sisgravadora/funcoes/login.php" target="_blank">Gravadora</a>
+                <a href="https://ceteia.guanambi.ifbaiano.edu.br/pswmedio/2023/2ai/sispapelaria/" target="_blank">Papelaria</a>
+                <a href="https://ceteia.guanambi.ifbaiano.edu.br/pswmedio/2023/2bi/sisesporte/esporte/" target="_blank">Esporte</a>
+                <a href="https://ceteia.guanambi.ifbaiano.edu.br/pswmedio/2023/2bi/sisrestaurante/" target="_blank">Restaurante</a>
+            </div>
+        </nav>
+
+        <main>
+            <div class="conteudo-main">
+                <h2>PROJETOS PROGRAMAÇÃO DE <br> SISTEMAS WEB</h2>
+
+                <div class="parte1">
+                    <div class="imagem">
+                        <img src="img/imagem05.jpg" alt="">
+                    </div>
+                    <p>Este projeto visa criar um ambiente colaborativo que conecta academia, micro e pequenas empresas, sociedade civil, e produtores rurais, promovendo inovação e empreendedorismo na região. O espaço oferecerá suporte básico para o desenvolvimento de projetos inovadores em diversas áreas do conhecimento, incluindo meio ambiente e agroecologia. Equipamentos voltados a medições ambientais, sistemas de irrigação e ferramentas de campo serão desenvolvidos para apoiar o trabalho dos produtores locais.</p>
+                </div>
+                <div class="parte2">
+
+                    <p> <strong>Visão de Futuro</strong> <br>
+
+                        Curto Prazo: Consolidar-se como um importante espaço de promoção de ciência, inovação e capacitação. <br>
+                        Médio Prazo: Contribuir para a formação de mão-de-obra qualificada e a execução de projetos que impulsionem a economia local. <br>
+                        Longo Prazo: Tornar-se um agente de desenvolvimento sustentável, alinhado ao ecossistema regional.</p>
+                    <div class="imagem">
+                        <img src="img/imagem04.jpg" alt="">
+                    </div>
+                </div>
+                
+            </div>
+        </main>
+        <aside>
+            <div>
+                <div class="imagem">
+                    <img src="img/imagem01.png" alt="">
+                </div>
+                <div class="imagem">
+                    <img src="img/imagem02.jpg" alt="">
+                </div>
+            </div>
+        </aside>
+        <footer>
+            <div class="conteiner">
+                <div class="imgs-logo">
+                    <a href="#"><img src="img/facebook.png" alt=""></a>
+                    <a href="#"><img src="img/instagram.png" alt=""></a>
+                    <a href="#"><img src="img/linkedin.png" alt=""></a>
+                    <a href="#"><img src="img/github.png" alt=""></a>
+                </div>
+                <div class="content-direitos">
+                    <span>@2024 Copyright: Erick Gustavo</span>
+                </div>
+            </div>
+            
+            <div class="footer-logo-if">
+                <img src="img/logo-if.png" alt="">
+            </div>
+
+        </footer>
+    </section>
+   
+
+</body>
+</html>
